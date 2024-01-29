bfcb515b435d2b1c09a3ec38059ae2edd78132c5
Imayhaveborkedit <imayhaveborkedit@users.noreply.github.com>
2024-01-28 22:38:45 -0500

    Prettify tiles and add navmaps
    Just adding all the corner and edge tiles to everything and setting up
    their nav maps.  Also moved trees to their own layer which also blocks
    movement.

--- DOMINIONS8/scripts/battlefield1.gd
+++ DOMINIONS8/scripts/battlefield1.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+@onready var tilemap: TileMap = $TileMap
+
+
 func _unhandled_key_input(event: InputEvent):
 	#print_debug("Got input event ", event)
 	if event is InputEventKey:
@@ -10,3 +13,9 @@ func _unhandled_key_input(event: InputEvent):
 			# F9 - Reload scene
 			KEY_F9 when event.pressed and not event.echo:
 				get_tree().reload_current_scene()
+			# F1 - Show tilemap navmap
+			KEY_F1 when event.pressed and not event.echo:
+				if tilemap.navigation_visibility_mode == TileMap.VISIBILITY_MODE_FORCE_SHOW:
+					tilemap.navigation_visibility_mode = TileMap.VISIBILITY_MODE_DEFAULT
+				else:
+					tilemap.navigation_visibility_mode = TileMap.VISIBILITY_MODE_FORCE_SHOW
